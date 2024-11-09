8fcc596b198cd05fb76020e634a577d3666ec8d8
F.Orellana <fernando_ariel1995@hotmail.com>
2024-11-09 03:40:09 -0300

    conectar back con front

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://distribuidora-back-production.up.railway.app/api',
 });
 
 export default api;
