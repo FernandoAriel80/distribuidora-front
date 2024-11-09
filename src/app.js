cc9b6eab407d0a8fb836d555d59e55c1fc44c3ca
F.Orellana <fernando_ariel1995@hotmail.com>
2024-11-09 17:22:01 -0300

    commit para railway

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://distribuidora-back-production.up.railway.app',
+    baseURL: 'https://distribuidora-back-production.up.railway.app/',
 });
 
 export default api;
