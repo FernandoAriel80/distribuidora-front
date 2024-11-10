46ee1652b8d190feeba4a04c44d39a4f5c00d954
F.Orellana <fernando_ariel1995@hotmail.com>
2024-11-09 22:21:48 -0300

    solo acomode unas cosas del front

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Lista de Productos</h1>
         <ul>
-            <li v-for="product in products" :key="product.id">
+            <li v-for="product in products.products" :key="product.id">
                 {{ product.name }} - ${{ product.price }}
             </li>
         </ul>
@@ -18,7 +18,7 @@ const products = ref([]);
 const fetchProducts = async () => {
     try {
         const response = await api.get('/api/products');
-        products.value = response.data.product;
+        products.value = response.data;
     } catch (error) {
         console.error('Error fetching products:', error);
     }
