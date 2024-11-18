038008088b1fa112da607385164fc16befb0bc82
F.Orellana <fernando_ariel1995@hotmail.com>
2024-11-18 14:30:49 -0300

    agregando para deply

---
+++ src/components/ProductList.vue
@@ -0,0 +1,21 @@
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  products: {
+    type: Array,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }}
+      </li>
+    </ul>
+  </template>
+  
