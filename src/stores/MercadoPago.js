94c1d4f2ff36ed9590508d3082859b063ddea09c
F.Orellana <fernando_ariel1995@hotmail.com>
2024-12-19 19:22:54 -0300

    cambios en mercado pago.js

--- src/stores/MercadoPago.js
+++ src/stores/MercadoPago.js
@@ -1,8 +1,19 @@
           
 import { loadMercadoPago } from "@mercadopago/sdk-js";
 
-await loadMercadoPago();
+/* await loadMercadoPago();
 export const mp = new MercadoPago('TEST-2307bb04-530a-4443-a20f-a7485cc97d5e',{ 
     locale: 'es-AR'
 });
+ */
+let mp; // Define `mp` en el ámbito global del módulo.
 
+(async function initialize() {
+  await loadMercadoPago();
+  mp = new MercadoPago('TEST-2307bb04-530a-4443-a20f-a7485cc97d5e', { 
+    locale: 'es-AR',
+  });
+})();
+
+// Exporta `mp` para que esté disponible fuera del archivo.
+export { mp };
