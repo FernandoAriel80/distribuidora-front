34760141015122e95cf172c4d02e5bf83c571cf6
F.Orellana <fernando_ariel1995@hotmail.com>
2024-12-19 17:44:54 -0300

    corrigiendo error de deploy del front para render

--- src/stores/MercadoPago.js
+++ src/stores/MercadoPago.js
@@ -1,14 +1,20 @@
           
 import { loadMercadoPago } from "@mercadopago/sdk-js";
 
-/* await loadMercadoPago();
+loadMercadoPago();
 export const mp = new MercadoPago('TEST-2307bb04-530a-4443-a20f-a7485cc97d5e',{ 
     locale: 'es-AR'
-}); */
+});
+
+/* let mp; // Define `mp` en el ámbito global del módulo.
 
 (async function initialize() {
-    await loadMercadoPago();
-    const mp = new MercadoPago('TEST-2307bb04-530a-4443-a20f-a7485cc97d5e', { 
-      locale: 'es-AR',
-    });
-  })();
+  await loadMercadoPago();
+  mp = new MercadoPago('TEST-2307bb04-530a-4443-a20f-a7485cc97d5e', { 
+    locale: 'es-AR',
+  });
+})();
+
+// Exporta `mp` para que esté disponible fuera del archivo.
+export { mp }; */
+
